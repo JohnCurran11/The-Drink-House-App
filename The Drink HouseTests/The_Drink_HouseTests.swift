f19db2ed8ffff5a45de4f8b1edcd2010e7738f05
John Curran <johncurny@outlook.com>
2018-11-09 18:01:11 +0000

    Initial Commit

---
+++ The Drink HouseTests/The_Drink_HouseTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Drink_HouseTests.swift
+//  The Drink HouseTests
+//
+//  Created by John Curran on 09/11/2018.
+//  Copyright © 2018 mixieworld. All rights reserved.
+//
+
+import XCTest
+@testable import The_Drink_House
+
+class The_Drink_HouseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
